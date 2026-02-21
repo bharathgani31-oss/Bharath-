22f4e37408e7b368fc267ad049408a24d948ea10
Bharath.c <Bharath.c@192.168.1.33>
2026-02-21 15:18:45 +0530

    Committing variables bc_variables.py

--- Bharath_Project/bharath_demo/bc_variables.py
+++ Bharath_Project/bharath_demo/bc_variables.py
@@ -2,5 +2,24 @@
 Created on 19 Feb 2026
 
 @author: Bharath.c
+
+Variables: A name given to the memory location which stores value
+
+Invalid cases :
+
+1. value = variable_name EX  30 = g
+2. Variable name should
 '''
-Print("My name is Bharath")
+a = 10
+b = 15
+c = a + b 
+print(c)
+
+d, e = 6, 7 # Single line comment
+print(d)
+print(e)
+
+e = f = 10 # assigning single alue for multiple variables
+print(e)
+print(f)
+
