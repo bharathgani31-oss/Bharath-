8fae29efc34665bb9c95b0d410ff68da3fd18f24
Bharath.c <Bharath.c@192.168.1.33>
2026-02-22 13:29:36 +0530

    Data  types code

--- Bharath_Project/bharath_demo/bc_data_types.py
+++ Bharath_Project/bharath_demo/bc_data_types.py
@@ -2,4 +2,70 @@
 Created on 21 Feb 2026
 
 @author: Bharath.c
+
+Data-type: Catogory to which a data belongs to.
+
+Types of data:
+Fundamental data types:
+1. int 
+2. float
+3. boolean
+4. string
+5. complex numbers
 '''
+a = "bharath"
+print(a)
+print(type(a))
+
+h = 10
+print(h)
+print(type(h))
+
+b = 4.5
+print(b)
+print(type(b))
+
+c = True
+print(c)
+print(type(c))
+
+d = 4+7j
+print(d)
+print(type(d))
+
+# Type casting: Converting one data type into an other
+
+e = str(a)
+print(e)
+print(type(e))
+
+f = bool(a)
+print(f)
+print(type(f))
+
+g = str(h)
+print(g)
+print(type(g))
+
+i = complex(b)
+print(i)
+print(type(i))
+
+j = int(b)
+print(j)
+print(type(j))
+
+k = bool(b)
+print(k)
+print(type(k))
+
+l = str(b)
+print(l)
+print(type(l))
+
+m = complex(b)
+print(m)
+print(type(m))
+
+
+
