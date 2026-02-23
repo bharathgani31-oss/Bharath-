814985265e1853f08a226fef251b49927876937e
Bharath.c <Bharath.c@LAPTOP-S7594SJT>
2026-02-23 11:20:54 +0530

    Condition Statements

---
+++ Bharath_Project/bharath_demo/bc_condition_statements.py
@@ -0,0 +1,32 @@
+'''
+Created on 23 Feb 2026
+
+@author: Bharath.c
+
+Conditional Statements: Statements based on condition from which flow of program execution will be decided
+
+We define condition Using if statement
+
+Syntax: 
+
+If <condition>
+    code
+Indentation: Leading spaces<spaces given at the begenning statements)
+'''
+age = int(input("please enter your age:"))
+if age <= 3:
+    print("you are a baby")
+else:
+    pass
+if age <=12:
+    print("child")
+else:
+    pass
+if age<=17:
+    print("teen")
+else:
+    pass
+if age<=59:
+    print("Adult")
+else:
+    print("old")
