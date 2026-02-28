91fceee614547836ca941793736ae192560e59ca
Bharath.c <Bharath.c@192.168.1.34>
2026-02-28 16:28:26 +0530

    Looping statements

--- Bharath_Project/bharath_demo/bc_loops_statements.py
+++ Bharath_Project/bharath_demo/bc_loops_statements.py
@@ -10,10 +10,31 @@ count = 5
 while count > 0:
     print("My name is bharath")
     count = count - 1
-'''
+
 
 count = 5
 while count > 0:
     print("My name is bharath")
     # count = count + 1 # Increment
     count = count - 1
+
+
+count = 0
+while count <= 100:
+    print(count)
+    count = count + 1
+
+num = int(input("enter a number"))
+if num % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+'''
+n = 0
+while n <= 100:
+    n = n+1
+    if n % 2 == 0:
+        print(n)
+        
+        
